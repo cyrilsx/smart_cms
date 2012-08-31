0eae918d6e8a6340c057cc347f7e3a2687e25cca
Cyril C <cyril@nexu.org>
2012-08-31 22:10:18 +0200

    animation basic stuff

--- static/script/objects.js
+++ static/script/objects.js
@@ -4,6 +4,9 @@ function View() {
 
     this.draw = function(context) {
     }
+
+    this.animate = function() {
+    }
 }
 
 
@@ -17,10 +20,18 @@ function CircleView(radius, x, y, color) {
     this.y = y;
     this.color = color;
 
+    this.speedx = 0; // px/s 
+    this.speedy = 0; // px/s 
+
     this.draw = function(context) {
         context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
         context.fillStyle = this.color;
         context.fill();
+    }
+
+    this.animate = function(timeframe) {
+        this.x += (timeframe * this.speedx) / 1000;
+        this.y += (timeframe * this.speedy) / 1000;
     }
 
 }
@@ -38,6 +49,9 @@ function RectView(x, y, width, height, color) {
     this.border_color = null;
     this.line_width = 0;
     
+    this.speedx = 0; // px/s 
+    this.speedy = 0; // px/s 
+    
     this.draw = function(context) {
         context.fillStyle = this.color;
         context.fillRect(this.x, this.y, this.width, this.height);
@@ -48,8 +62,39 @@ function RectView(x, y, width, height, color) {
             context.strokeRect(this.x,  this.y, this.width, this.height);
         }
     }
+    
+    this.animate = function(timeframe) {
+        this.x += (timeframe * this.speedx) / 1000;
+        this.y += (timeframe * this.speedy) / 1000;
+    }
 }
 
+function ImageView(x, y, image, width, height) {
+    this.prototype = new View();
+    this.prototype.constructor = ImageView;
+    
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.image = new Image();
+    this.image.src = image;
+    this.rot = 0;
+    
+    this.speedx = 0; // px/s 
+    this.speedy = 0; // px/s 
+    
+    this.draw = function(context) {
+        context.rotate(this.rot * Math.PI / 180);
+        context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+    
+    this.animate = function(timeframe) {
+        this.x += (timeframe * this.speedx) / 1000;
+        this.y += (timeframe * this.speedy) / 1000;
+    }
+
+}
 
 function AnimationContext(canvas, width, height, timeframe,array_views) {
     this.canvas = canvas;
@@ -59,17 +104,21 @@ function AnimationContext(canvas, width, height, timeframe,array_views) {
     this.timeframe = timeframe;
     this.context = this.canvas.getContext("2d");
 
+    var self = this;
+
     this.clear = function() {
         this.context.clearRect(0, 0, this.width, this.height);
 
     }
 
     this.draw = function() {
-        this.clear();
+        self.clear();
 
-        for(var i = 0; i < this.views.length; i++) {
-            this.views[i].draw(this.context);
+        for(var i = 0; i < self.views.length; i++) {
+            self.views[i].draw(self.context);
+            self.views[i].animate(self.timeframe);
         }
+
     }
 
 }
--- static/script/script.js
+++ static/script/script.js
@@ -2,13 +2,17 @@
 
 function init() {
     canvas = document.getElementById('canvas');
+    rect = new RectView(10, 10, 100, 50, "#f00");
+    rect.speedx = 10;
     var views= [
-            new RectView(10, 10, 100, 50, "#f00"),
-            new CircleView(50, 100, 100, "#0f0")
+            rect,
+            //new RectView(10, 10, 100, 50, "#f00"),
+            new CircleView(50, 100, 100, "#0f0"),
+            new ImageView(10, 10, "/static/icon/bg.jpg",98, 48),
         ];
 
-    var anim = new AnimationContext(canvas, 500, 200, 10, views);
-    setInterval(anim.draw(), anim.timeframe);
+    anim = new AnimationContext(canvas, 500, 200, 10, views);
+    setInterval(anim.draw, anim.timeframe);
     return anim;
 }
 
